68a0a5571f55c106bac75a4f6f1c88cc3f4a52c1
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-13 14:32:44 +0000

    style: rounded corners to changelog (#2702)

--- src/lib/components/changelog/wrapper.svelte
+++ src/lib/components/changelog/wrapper.svelte
@@ -3,6 +3,8 @@
   export { clazz as class };
 </script>
 
-<div class="content-docs prose prose-h3:mb-6 max-w-none {clazz}">
+<div
+  class="content-docs prose-img:rounded-t-md md:prose-img:rounded-t-2xl prose prose-h3:mb-6 max-w-none {clazz}"
+>
   <slot />
 </div>
