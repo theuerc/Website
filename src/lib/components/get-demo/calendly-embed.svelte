bea6b4a6258110b8ddb8b41c60402d86123684f2
Siddhant Khare <siddhant@gitpod.io>
2022-10-20 11:51:16 +0530

    nit-fix: design bug (#2930)

--- src/lib/components/get-demo/calendly-embed.svelte
+++ src/lib/components/get-demo/calendly-embed.svelte
@@ -28,5 +28,5 @@
 <div
   class="calendly-inline-widget"
   data-url="https://calendly.com/d/d47-7n4-c63?primary_color=ffae33"
-  style="min-width:250px;height:726px;"
+  style="min-width:250px;height:780px;"
 />
