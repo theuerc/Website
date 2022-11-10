47369825f77482042c0aa56f2e754af46659b46a
Siddhant Khare <siddhant@gitpod.io>
2022-11-10 16:12:44 +0530

    nit-fix: followup - #3014 (#3015)

--- src/routes/vs/github-codespaces.svelte
+++ src/routes/vs/github-codespaces.svelte
@@ -8,8 +8,8 @@
   import Header from "$lib/components/header.svelte";
   import OpenGraph from "$lib/components/open-graph.svelte";
   import Section from "$lib/components/section.svelte";
-  //   import Spaces_1 from "$lib/components/svgs/github-codespaces/spaces-1.svelte";
-  import Spaces_2 from "$lib/components/svgs/github-codespaces/spaces-2.svelte";
+  // import Spaces_1 from "$lib/components/svgs/github-codespaces/spaces-1.svelte";
+  // import Spaces_2 from "$lib/components/svgs/github-codespaces/spaces-2.svelte";
   import FeatureTable from "$lib/components/ui-library/feature-table/feature-table.svelte";
   import {
     automationFirstFeature,
@@ -46,17 +46,6 @@
       paragraph:
         "Gitpod works out of the box with your favourite JetBrains IDEs and VS Code. Give your team the power to choose their own tools and avoid being locked in to a single vendor.",
       previewComponent: idesSvelte,
-    }}
-  />
-
-  <FeatureBox
-    feature={{
-      title: "Resource Efficient",
-      paragraph:
-        "Instead of running on expensive VMs, Gitpod provisions secure containers and achieves best-in-class resource-efficiency with scalable workspaces running on shared high-powered cloud servers. <a href='https://www.researchgate.net/publication/333096446_Reduced_carbon_emission_and_optimized_power_consumption_technique_using_container_over_virtual_machine' target='_blank'>Helping our planet</a> 🌱.",
-      previewComponent: Spaces_2,
-      footnote:
-        "Price comparison for a developer working 5h per day, 21 per month with 8 core CPUs and 12GB RAM. Last verified 3 Sep 2021. Sources: <a href='/pricing'>Gitpod</a>, <a href='https://github.com/features/codespaces' target='_blank'>GitHub Codespaces</a>.",
     }}
   />
 </FeatureBoxes>
