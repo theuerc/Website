27a8b70bd7ac6bbc7dd0c522b5404757827b9e3a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-17 15:49:36 +0000

    fix:card clickable (#3034)

--- src/lib/components/events/landing-page/webinar-card.svelte
+++ src/lib/components/events/landing-page/webinar-card.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import Card from "$lib/components/ui-library/card";
-  import LinkButton from "$lib/components/ui-library/link-button/link-button.svelte";
   export let webinar: {
     isInPast: boolean;
     text: string;
@@ -10,28 +9,34 @@
   };
 </script>
 
-<Card
-  size="small"
-  class="p-xx-small transition-all duration-200  delay-[50ms] flex flex-col overflow-hidden w-full max-w-sm"
->
-  <div class="-mt-xx-small -mx-xx-small">
-    <img class="w-full  object-cover" src={webinar.image} alt={webinar.alt} />
-  </div>
-  <div class="-mx-xx-small mt-[2.5rem] h-full">
-    <div class="px-xx-small flex flex-col h-full justify-between">
-      <p class="text-p-medium text-body">
-        {@html webinar.text}
-      </p>
-      <div>
-        <LinkButton
-          class="mt-micro"
-          sveltekit:prefetch
-          size="medium"
-          href={webinar.href}
-          variant={webinar.isInPast ? "cta" : "primary"}
-          >{webinar.isInPast ? "Watch recording" : "Sign up now"}</LinkButton
-        >
+<a sveltekit:prefetch class="no-underline" href={webinar.href}>
+  <Card
+    size="small"
+    class="p-xx-small hover:shadow-normal transition-all duration-200 delay-[50ms] flex flex-col overflow-hidden w-full h-full max-w-sm"
+  >
+    <div class="-mt-xx-small -mx-xx-small">
+      <img
+        class="w-full h-full object-cover"
+        src={webinar.image}
+        alt={webinar.alt}
+      />
+    </div>
+    <div class="-mx-xx-small h-full mt-[2.5rem]">
+      <div class="px-xx-small flex h-full flex-col justify-between">
+        <p class="text-p-medium text-body">
+          {@html webinar.text}
+        </p>
+        <div>
+          <div
+            class="mt-micro  transition-all duration-200 delay-[50ms] inline-block py-2 px-6 text-btn-small leading-4 shadow-light rounded-xl  font-semibold whitespace-nowrap 
+              {webinar.isInPast
+              ? 'text-black bg-sand-dark dark:bg-light-black dark:text-sand-dark'
+              : 'bg-primary text-important dark:text-black'}"
+          >
+            <p>{webinar.isInPast ? "Watch recording" : "Sign up now"}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</Card>
+  </Card>
+</a>
