277a041e4542ff8cc2c5c114200076fa9f817e97
George Tsiolis <tsiolis.g@gmail.com>
2022-12-15 19:06:53 +0200

    Replace prebuild message emoji (#3165)
    Parallel fix with gitpod-io/gitpod#15367.

--- src/lib/contents/terminal.ts
+++ src/lib/contents/terminal.ts
@@ -104,7 +104,7 @@ export const terminalSource = `\u001b[33mGitpod\u001b[39m \u001b[34m/Workspace\u
   \u001b[33mFinished\u001b[39m dev target(s) in 12m 30s
 
  exit
- 🤙 This task ran as a workspace prebuild
+ 🍊 This task ran as a workspace prebuild
  ⏱️ Well done on saving 12 minutes
 
 \u001b[33mGitpod\u001b[39m \u001b[34m/Workspace\u001b[39m $ `;
