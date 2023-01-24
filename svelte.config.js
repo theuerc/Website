3c429d4a1c2a4d9f4c7d0333ed65a9e50c99383d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-01-24 07:48:11 +0000

    feat: Sveltekit Upgrade (#3277)

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { mdsvex } from "mdsvex";
 import headings from "remark-autolink-headings";
 import remarkExternalLinks from "remark-external-links";
 import slug from "remark-slug";
-import sveltePreprocess from "svelte-preprocess";
+import preprocess from "svelte-preprocess";
 import remarkSetImagePath from "./src/lib/utils/remark-set-image-path.js";
 import remarkEmbedVideo from "./src/lib/utils/remark-embed-video.js";
 import remarkLinkWithImageAsOnlyChild from "./src/lib/utils/remark-link-with-image-as-only-child.js";
@@ -14,38 +14,16 @@ import rehypeImgSize from "rehype-img-size";
 import { highlightCode } from "./src/lib/utils/highlight.js";
 import { mdsvexGlobalComponents } from "./src/lib/utils/mdsvex-global-components.js";
 import { h } from "hastscript";
-import { execSync } from "node:child_process";
 import { visit } from "unist-util-visit";
 
 /** @type {Partial<import('vite').ServerOptions>} */
-let extendedViteServerOptions;
-
-try {
-  /**
-   * When changing this port number, remember to update it also in these files:
-   * - .gitpod.yml
-   * - cypress.json
-   */
-  const port = 3000;
-
-  const gitpodPortUrl = execSync(`gp url ${port}`).toString().trim();
-
-  extendedViteServerOptions = {
-    port,
-    hmr: {
-      protocol: "wss",
-      host: new URL(gitpodPortUrl).hostname,
-      clientPort: 443,
-    },
-  };
-} catch {
-  extendedViteServerOptions = {};
-}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
-
+  compilerOptions: {
+    hydratable: true,
+  },
   kit: {
     browser: {
       hydrate: true,
@@ -56,7 +34,6 @@ const config = {
       edge: false,
       split: false,
     }),
-    appDir: "_app",
     files: {
       assets: "static",
       hooks: "src/hooks",
@@ -64,28 +41,22 @@ const config = {
       routes: "src/routes",
       template: "src/app.html",
     },
+
     prerender: {
       crawl: true,
       enabled: true,
       onError: "fail", //once the netlify-endpoint for requesting the images isn't needed anymore this can be "fail" again
       entries: ["*"],
     },
-    vite: {
-      resolve: {
-        preserveSymlinks: true,
-      },
-      server: {
-        fs: {
-          allow: [".."],
-        },
-        ...extendedViteServerOptions,
-      },
-    },
   },
 
   // options passed to svelte.preprocess (https://svelte.dev/docs#svelte_preprocess)
   preprocess: [
-    sveltePreprocess({ postcss: true, scss: true, preserve: ["ld+json"] }),
+    preprocess({
+      postcss: true,
+      scss: true,
+      preserve: ["ld+json"],
+    }),
     mdsvexGlobalComponents({
       dir: `$lib/components`,
       list: [["CodeFence", "code-fence.svelte"]],
---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { execSync } from "node:child_process";
+
+/**
+ * @type {Partial<import("vite").ServerOptions>}
+ */
+let extendedViteServerOptions;
+try {
+  const port = 3000;
+
+  const gitpodPortUrl = execSync(`gp url ${port}`).toString().trim();
+
+  extendedViteServerOptions = {
+    port,
+    hmr: {
+      protocol: "wss",
+      host: new URL(gitpodPortUrl).hostname,
+      clientPort: 443,
+    },
+  };
+} catch {
+  extendedViteServerOptions = {};
+}
+
+/**
+ * @type {import("vite").UserConfig}
+ */
+const config = {
+  plugins: [sveltekit()],
+  resolve: {
+    preserveSymlinks: true,
+  },
+  server: {
+    fs: {
+      allow: [".."],
+    },
+    ...extendedViteServerOptions,
+  },
+};
+
+export default config;
