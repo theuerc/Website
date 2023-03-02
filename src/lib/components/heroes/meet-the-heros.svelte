247e9f69cc42618e0a9b56bf5491ae7800bded80
Siddhant Khare <siddhant@gitpod.io>
2023-03-02 16:34:14 +0530

    :rocket: Gitpod Community Heroes 🧡 (#3479)
    * :rocket: Gitpod Community Heroes 🧡
    
    * :wrench: add demo image
    
    * :sparkles: Add Images of Community Heroes
    
    * nit css fixes
    
    * remove `socialMediaUrl` & addn. Discord usernames

--- src/lib/components/heroes/meet-the-heros.svelte
+++ src/lib/components/heroes/meet-the-heros.svelte
@@ -1,9 +1,11 @@
 <script>
   import Section from "../section.svelte";
   import LinkButton from "$lib/components/ui-library/link-button";
+  import { heroes } from "$lib/contents/heroes/community-heroes";
+  import Heroes from "./heroes-list.svelte";
 </script>
 
-<Section class="text-center">
+<Section class="text-center" id="community-heroes">
   <h2 class="h2">Meet the Heroes</h2>
   <p class="text-large mb-x-small">
     Would you like to connect with the Community Heroes? Have a chat on Discord.
@@ -14,4 +16,5 @@
     href="https://www.gitpod.io/chat"
     target="_blank">Connect on Discord</LinkButton
   >
+  <Heroes {heroes} />
 </Section>
