a5f7b8fece4c26f362116030ba0f6af22ceae27f
Siddhant Khare <siddhant@gitpod.io>
2023-05-16 16:12:38 +0530

    :wrench: followup #3643 (#3646)

--- src/lib/components/changelog/changelog-date.svelte
+++ src/lib/components/changelog/changelog-date.svelte
@@ -6,7 +6,10 @@
 </script>
 
 <div class="w-full md:w-4/12">
-  <ChangelogLink class="mb-xx-small" {href}>
+  <ChangelogLink
+    class="mb-xx-small !text-2xl !text-[#999795] !font-normal"
+    {href}
+  >
     {date}
   </ChangelogLink>
 </div>
