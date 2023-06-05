10eca1c29ed34675915c9d2665a863b4024a43c9
Siddhant Khare <siddhant@gitpod.io>
2023-06-05 19:10:28 +0530

    minor design fix (#3720)

--- src/lib/components/team/core-values.svelte
+++ src/lib/components/team/core-values.svelte
@@ -19,7 +19,7 @@
     >
     <div class="grid grid-cols-1 md:grid-cols-2 max-w-3xl gap-micro">
       {#each coreValues as card}
-        <Card {card} />
+        <Card {card} textAlign="left" />
       {/each}
     </div>
   </div>
