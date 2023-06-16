cd1c130e6bed8b9fa8c80ef93137e7174e080b4b
Siddhant Khare <siddhant@gitpod.io>
2023-06-16 10:15:02 +0530

    🐛 fix while handling Changelog Entries in Hooks (#3751)

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,24 +4,47 @@ import { sequence } from "@sveltejs/kit/hooks";
 import { compile } from "mdsvex";
 import { basename } from "path";
 
+// This function fetches changelog entries from the /src/lib/contents/changelog/ directory
+// and adds them to the event.locals object. It also orders them by date.
+
 const handleChangelogEntries = async ({ event, resolve }) => {
-  const changelogEntries = await Promise.all(
-    Object.entries(import.meta.glob("/src/lib/contents/changelog/*.md"))
-      .filter(([path]) => !path.endsWith("_template.md"))
-      .map(async ([path, mod]) => {
-        const { default: content, metadata } = await mod();
-        const fileName = basename(path);
-        return {
-          ...metadata,
-          excerpt: (await compile(metadata.excerpt)).code.trim(),
-          content: content.render().html,
-          fileName,
-        };
-      })
-  );
-  changelogEntries.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
-  event.locals.changelogEntries = changelogEntries;
-  return await resolve(event);
+  try {
+    // Get all the changelog entries from the glob.
+    const changelogEntries = await Promise.all(
+      Object.entries(import.meta.glob("/src/lib/contents/changelog/*.md"))
+        // Filter out the template.
+        .filter(([path]) => !path.endsWith("_template.md"))
+        // Convert each entry into metadata.
+        .map(async ([path, mod]) => {
+          // Get the content and metadata from the module.
+          const { default: content, metadata } = (await mod()) as any;
+          // Get the file name from the path.
+          const fileName = basename(path);
+          // Compile the excerpt.
+          const excerpt = await compile(metadata.excerpt);
+          // Return the metadata with the compiled excerpt and the rendered content.
+          return {
+            ...metadata,
+            excerpt: excerpt.code.trim(),
+            content: content.render().html,
+            fileName,
+          };
+        })
+    );
+    // Sort the changelog entries by date.
+    changelogEntries.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+    // Pass the changelog entries to the next handler.
+    event.locals.changelogEntries = changelogEntries;
+    // Pass the event to the next handler.
+    return await resolve(event);
+  } catch (error) {
+    // If there was an error, log it.
+    console.error(error);
+    // Return a response with the error.
+    return new Response(`Internal Server Error\n ${error.message}`, {
+      status: 500,
+    });
+  }
 };
 
 const handleHeaders = async ({ event, resolve }) => {
